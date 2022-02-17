c0b5335fa483c0b3d90b4f109eb2e0c7cfb4d519
mati <matiexe31@gmail.com>
2022-02-17 16:38:54 -0300

    se valida la extesion y se cambia el nombre final de los archivos cargados

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,5 +1,6 @@
 const path = require('path')
-
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
 const { response } = require("express");
 
 const cargaDeArchivo = (req , res = response)=>{
@@ -16,8 +17,22 @@ const cargaDeArchivo = (req , res = response)=>{
 
 
     const { archivo } = req.files;
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length-1];
+    const extensionesValidaz =[
+        'jpg',
+        'png',
+        'gif',
+        'jpeg'
+    ]
+    if(extensionesValidaz.includes(extension)){
+        res.status(400).json({
+            msg :`La exstension ${extension} no es permitida`
+        })
+    }
 
-    uploadPath = path.join(__dirname ,'../uploads/', archivo.name);
+    const nombreTemp = uuidv4() + '.'+ extension;
+    uploadPath = path.join(__dirname ,'../uploads/', nombreTemp);
 
     archivo.mv(uploadPath,(err)=>{
         if(err){
