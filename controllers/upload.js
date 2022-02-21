178dc986b5002b8dc4c301bfa14c82a8b14d0573
mati <matiexe31@gmail.com>
2022-02-21 11:15:39 -0300

    se envia imagen de relleno cuando no tenga imagen asignada

--- controllers/upload.js
+++ controllers/upload.js
@@ -71,7 +71,54 @@ const actualizarArchivo = async(req, res = response)=>{
     }
     
 }
+
+const mostrarImagen = async(req, res = response) =>{
+    
+    const{id, coleccion}= req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = Usuario.findById(id);
+            if(!modelo){
+                return res.status(400).json({
+                    msg:`no existe un usuario con el id ${id}`
+                })
+            }
+        
+        break;
+        case 'producto':
+            modelo = Producto.findById(id);
+            if(!modelo){
+                return res.status(400).json({
+                    msg:`no existe un producto con el id ${id}`
+                })
+            }
+        
+        break;
+    
+        default:
+            return res.status(500).json({msg:'se me olvido validar esto'});
+    }
+
+    // Limpiar imagenes previas
+
+    try {
+        if(model.img){
+            const pathImagen =  path.join( __dirname,'../uploads,',coleccion,model.img);
+            if(fs.existsSync(pathImagen)){
+                return res.sendFile(pathImagen)
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    res.sendFile(__dirname,'../assets/no-image.jpg');
+}
+
 module.exports={
     cargaDeArchivo,
-    actualizarArchivo
+    actualizarArchivo,
+    mostrarImagen
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check  } = require('express-validator');
-const { cargaDeArchivo, actualizarArchivo } = require('../controllers/upload');
+const { cargaDeArchivo, actualizarArchivo, mostrarImagen } = require('../controllers/upload');
 const { validarColecciones } = require('../helpers');
 const { validarCampos, validarArchivosubir } = require('../middlewares');
 
@@ -15,6 +15,13 @@ router.put('/:coleccion/:id',[
     validarCampos
 
 ],actualizarArchivo)
+
+router.get('/:coleccion/:id',[
+    validarArchivosubir,
+    check('id','El id deber ser valido').isMongoId(),
+    check('coleccion').custom(c=>validarColecciones(c,['usuarios','productos'])),
+    validarCampos
+],mostrarImagen)
 
 
 module.exports = router
